244a34a7c42c1fe809d24d9aa283da30873b02c8
Jinpeng Shi <login@jpshi.me>
2024-10-20 19:31:16 +0800

    chore(remark-plugin): 添加插件 `remark-toc` `remark-wiki-link`

---
+++ astro.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+
+import remarkToc from "remark-toc";
+import remarkWikiLink from 'remark-wiki-link';
+
+// https://astro.build/config
+export default defineConfig({
+    site: "https://jnpngshiii.github.io/",
+    base: "/",
+    integrations: [sitemap()],
+    markdown: {
+        shikiConfig: {
+            theme: "material-theme-darker",
+            langs: [],
+        },
+
+        remarkPlugins: [
+            remarkToc,
+            [
+                remarkWikiLink,
+                {
+                    aliasDivider: '|',
+                    pageResolver: (name: String) => [name.replace(/ /g, '-').toLowerCase()],
+                    hrefTemplate: (permalink: String) => `/posts/${permalink.replace('#', '/#')}`,
+                }
+
+            ],
+        ],
+    },
+});
