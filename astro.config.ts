fa4769900d69c7404ae81d53e30f674052077864
Jinpeng Shi <login@jpshi.me>
2024-10-20 19:36:51 +0800

    feat(ui): 更新 `shiki` 主题

--- astro.config.ts
+++ astro.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     integrations: [sitemap()],
     markdown: {
         shikiConfig: {
-            theme: "material-theme-darker",
+            theme: "rose-pine-dawn",
             langs: [],
         },
 
