c890b82a775f01889e6b09e6158d3b79b46c46d3
Jinpeng Shi <login@jpshi.me>
2024-10-20 19:16:00 +0800

    feat(ui): 个性化博客

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Cassidy's blog template";
-export const SITE_DESCRIPTION = "A blog template for you!";
+export const SITE_TITLE = "Jnpng Shiii 的博客";
+export const SITE_DESCRIPTION = "记录学习, 记录思考";
